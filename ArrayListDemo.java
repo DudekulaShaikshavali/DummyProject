b1ecd51d28a76674c7b3fa6acb2a6537d09b256e
Shaikshavali <shaikshasiddu97@gmail.com>
2024-06-25 12:38:59 +0530

    java file is committed

---
+++ ArrayListDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+       ArrayList list=new ArrayList();
+       list.add(5);
+       list.add(50.0f);
+       list.add(896702345);
+       list.add('a');
+       list.add("mango");
+       list.add(true);
+       System.out.println(list);
+            
+            
+    }
+}
